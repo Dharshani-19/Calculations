e3519bb36198e951bcef9aa27feb17f8456dd590
Dharshani-19 <dharshani.a@mitrahsoft.in>
2026-03-19 12:35:34 +0530

    branch added

---
+++ string.js
@@ -0,0 +1,18 @@
+function reverse(data){
+let a=data.split("").reverse().join("");
+console.log(a);
+
+}
+reverse("javascript")
+
+function trim(value){
+let a=value.trim();
+console.log(a);
+}
+trim("    JavaScript   ")
+
+function capitalize(values){
+    let a=values.toUpperCase();
+    console.log(a);
+}
+capitalize("javascript");
